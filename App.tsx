52f9830321c3a10e5c851935f3c075b84a904ffa
viniciussec <viniciussec@hotmail.com>
2023-06-02 20:23:17 -0300

    feat: Adicionando tailwind ao projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className="items-center justify-center flex-1 w-full bg-gray-800">
+      <View className="justify-center h-40 p-4 bg-white rounded-md">
+        <Text className="">Teste</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
