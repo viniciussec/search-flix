1ca7cd5129fc1a86f40e5ef0b2233132c0949536
viniciussec <viniciussec@hotmail.com>
2023-06-26 20:22:48 -0300

    feat: Formatação das datas e linguagem em pt-BR

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,21 +1,24 @@
 import { Image, Text, TouchableOpacity, View } from "react-native";
-import { IMAGE_URL } from "../constants";
+import { IMAGE_URL } from "../utils/constants";
 import { Movie } from "../types/Movie";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../App";
+import { formatDate } from "../utils/formatDate";
 
 type Props = {
   item: Movie;
 };
 
 export default function MovieCard({ item }: Props) {
-  const route = useRoute();
-  const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
   return (
-    <TouchableOpacity className="mt-4 flex flex-row space-x-4 h-64 border-2 border-[#323B45] rounded-md p-4"
-      onPress={() => navigation.navigate("MovieDetails", { movieId: item.id })}>
+    <TouchableOpacity
+      className="mt-4 flex flex-row space-x-4 h-64 border-2 border-[#323B45] rounded-md p-4"
+      onPress={() => navigation.navigate("MovieDetails", { movieId: item.id })}
+    >
       <Image
         source={{ uri: IMAGE_URL + item.poster_path }}
         className="w-32 h-full"
@@ -23,7 +26,9 @@ export default function MovieCard({ item }: Props) {
       <View className="justify-between">
         <View className="w-48">
           <Text className="text-2xl text-white">{item.original_title}</Text>
-          <Text className="text-gray-400 text-md">{item.release_date}</Text>
+          <Text className="text-gray-400 text-md">
+            {formatDate(item.release_date)}
+          </Text>
         </View>
         <Text className="text-5xl text-green-500">
           {item.vote_average * 10}%
