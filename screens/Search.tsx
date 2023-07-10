3b7bb331b614752a46f552c4f955d4d5ad253c3e
viniciussec <viniciussec@hotmail.com>
2023-07-10 20:06:11 -0300

    feat: Modificações visuais envolvendo filtro

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { Ref, useEffect, useRef, useState } from "react";
 import {
   ScrollView,
   Text,
@@ -15,37 +15,39 @@ import MovieCard from "../components/MovieCard";
 import { Genre } from "../types/Genre";
 import SelectDropdown from "react-native-select-dropdown";
 
-type OrderingMode = null | "vote_average" | "views" | "release_date"
+type OrderingMode = null | "vote_average" | "views" | "release_date";
 
 export default function Info() {
   const [filterModal, setFilterModal] = useState(false);
   const [movies, setMovies] = useState<Movie[]>([]);
   const [order, setOrder] = useState<OrderingMode>(null);
   const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [hasFilter, setHasFilter] = useState(false);
 
-  const [year, setYear] = useState("")
-  const [genres, setGenres] = useState<Genre[]>([])
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null)
-  const genreDropdownRef = useRef(null)
+  const [year, setYear] = useState("");
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const genreDropdownRef = useRef<any>(null);
 
   function clearAll() {
     setOrder(null);
-    setYear("")
+    setYear("");
     setSelectedGenre(null);
-    //@ts-ignore
-    genreDropdownRef.current.reset()
+    setHasFilter(false);
+    genreDropdownRef?.current?.reset();
   }
 
-  function handleApplyFilters(){
-    loadMovies()
-    setFilterModal(false)
+  function handleApplyFilters() {
+    loadMovies();
+    setFilterModal(false);
   }
 
   async function loadMovies() {
-    let url = `search/movie?language=pt-BR&page=1&query=${search}`
-    if (year) url = url + `&primary_release_year=${year}`
+    let url = `search/movie?language=pt-BR&page=1&query=${search}`;
+    if (year) url = url + `&primary_release_year=${year}`;
     const response = await API.get(url);
-    const filteredMovies = filterMovies(response.data.results)
+    const filteredMovies = filterMovies(response.data.results);
     setMovies(filteredMovies);
   }
   function loadGenreList() {
@@ -79,7 +81,7 @@ export default function Info() {
             placeholder="Digite aqui para procurar"
             className="w-full h-10 px-4 bg-white rounded-3xl"
             onChange={(e) => setSearch(e.nativeEvent.text)}
-            onBlur={()=>loadMovies()}
+            onBlur={() => loadMovies()}
           ></TextInput>
           <TouchableOpacity
             onPress={() => loadMovies()}
@@ -92,9 +94,24 @@ export default function Info() {
           onPress={() => setFilterModal(true)}
           className="flex-row items-center w-full mt-4"
         >
-          <Entypo name="select-arrows" size={30} color="white" />
-          <Text className="text-white text-md">Filtros e ordenação</Text>
+          <Entypo
+            name="select-arrows"
+            size={30}
+            color={hasFilter ? "rgb(34 197 94)" : "white"}
+          />
+          <Text
+            className={
+              "text-md " + (hasFilter ? "text-green-500" : "text-white")
+            }
+          >
+            Filtros e ordenação
+          </Text>
         </TouchableOpacity>
+        {movies.length === 0 && (
+          <View className="items-center justify-center mt-10">
+            <Text className="text-lg text-white">Sem resultados</Text>
+          </View>
+        )}
         <ScrollView showsVerticalScrollIndicator={false}>
           <View className="items-center justify-center mb-20 space-y-10">
             {movies.map((item, index) => (
@@ -115,40 +132,47 @@ export default function Info() {
                 defaultButtonText="Gênero"
                 data={genres}
                 ref={genreDropdownRef}
-                onSelect={(selectedItem) => setSelectedGenre(selectedItem)}
+                onSelect={(selectedItem) => {
+                  setSelectedGenre(selectedItem);
+                  setHasFilter(true);
+                }}
                 buttonTextAfterSelection={(selectedItem) => selectedItem.name}
                 rowTextForSelection={(item) => item.name}
                 defaultValue={selectedGenre}
                 buttonStyle={{
-                  backgroundColor: 'rgb(22 163 74)',
+                  backgroundColor: selectedGenre
+                    ? "rgb(22 163 74)"
+                    : "rgb(20 83 45)",
                   borderRadius: 30,
-                  width: '100%',
+                  width: "100%",
                   height: 30,
                 }}
                 buttonTextStyle={{
-                  color: '#fff',
+                  color: "#fff",
                   fontSize: 14,
                 }}
                 dropdownStyle={{
-                  backgroundColor: 'rgb(31 41 55)'
+                  backgroundColor: "rgb(31 41 55)",
                 }}
                 rowTextStyle={{
-                  color: '#fff'
+                  color: "#fff",
                 }}
               />
-              <TextInput className=" 
-                  items-center justify-center w-full px-6 bg-green-600 rounded-3xl 
-                text-white text-md"
-                placeholder="ano"
+              <TextInput
+                className={
+                  "items-center justify-center w-full px-6 text-white rounded-3xl text-md " +
+                  (year ? "bg-green-600" : "bg-green-800")
+                }
+                placeholder="Ano"
                 value={year}
-                onChange={(e) => setYear(e.nativeEvent.text.replace(/[^0-9]/g, ''))}
+                onChange={(e) => {
+                  setYear(e.nativeEvent.text.replace(/[^0-9]/g, ""));
+                  setHasFilter(true);
+                }}
                 keyboardType="numeric"
-                placeholderTextColor="#DDD"
+                placeholderTextColor="#FFF"
                 textAlign="center"
               />
-              <TouchableOpacity className="items-center justify-center w-full px-6 py-1 bg-green-600 rounded-3xl">
-                <Text className="text-white text-md">Classificação</Text>
-              </TouchableOpacity>
             </View>
             <View className="w-0.5 h-full bg-gray-400 rounded-xl"></View>
             <View className="flex flex-col flex-1 w-1/2 space-y-4">
@@ -164,7 +188,10 @@ export default function Info() {
           <Text className="my-4 text-xl text-white">Ordenação</Text>
           <View className="items-center space-y-4">
             <TouchableOpacity
-              onPress={() => setOrder("vote_average")}
+              onPress={() => {
+                setOrder("vote_average");
+                setHasFilter(true);
+              }}
               className={
                 "items-center justify-center w-1/2 px-4 py-1 rounded-3xl " +
                 (order === "vote_average" ? "bg-green-600" : "bg-green-900")
@@ -173,7 +200,10 @@ export default function Info() {
               <Text className="text-white text-md">Nota</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              onPress={() => setOrder("views")}
+              onPress={() => {
+                setOrder("views");
+                setHasFilter(true);
+              }}
               className={
                 "items-center justify-center w-1/2 px-4 py-1 rounded-3xl " +
                 (order === "views" ? "bg-green-600" : "bg-green-900")
@@ -182,7 +212,10 @@ export default function Info() {
               <Text className="text-white text-md">Visualizações</Text>
             </TouchableOpacity>
             <TouchableOpacity
-              onPress={() => setOrder("release_date")}
+              onPress={() => {
+                setOrder("release_date");
+                setHasFilter(true);
+              }}
               className={
                 "items-center justify-center w-1/2 px-4 py-1 rounded-3xl " +
                 (order === "release_date" ? "bg-green-600" : "bg-green-900")
